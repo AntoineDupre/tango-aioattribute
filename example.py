1c00dd9025eeac728582d9efb6b30d73ee9cadc2
Antoine Dupré <antoine.dupre@maxiv.lu.se>
2019-09-20 17:33:26 +0200

    Update example.py

--- example.py
+++ example.py
@@ -3,7 +3,7 @@ from aioattribute.manager import SubscriptionManager
 from loguru import logger
 
 
-async def subscribe_and_listen(no, names):
+async def subscribe_and_listen(names):
 
     async with mgr.attribute_reads(names) as attribute_reads:
         async for read in attribute_reads:
@@ -12,7 +12,6 @@ async def subscribe_and_listen(no, names):
 
 async def main():
     await subscribe_and_listen(
-        1,
         [
             "antdup/tangotest/1/ampli",
             "antdup/tangotest/1/double_scalar",
@@ -20,7 +19,7 @@ async def main():
             "antdup/tangotest/2/ampli",
             "antdup/tangotest/2/double_scalar",
             "antdup/tangotest/2/State",
-        ],
+        ]
     )
 
 
